0707d3f32d7f1107b661a441093af7101a17b55a
Germain Bonnard <germain80420@gmail.com>
2023-03-23 21:05:17 +0100

    modif dom card photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -2,13 +2,13 @@ function photographerFactory(data) {
     //on créé un objet Photographer en envoyant les données de l'objet json en parametre
     const photographer = new Photographer(data);
 
-    const picture = `assets/photographers/${photographer._portrait}`;
+    const picture = `assets/photographers/${photographer.portrait}`;
 
     function getUserCardDOM() {
 
         //on crée et on ajoute les différents éléments du dom pour l'affichage des informations 
         const linkPhotographer = document.createElement('a');
-        linkPhotographer.href=`/photographer.html?id=${photographer._id}`;
+        linkPhotographer.href=`/photographer.html?id=${photographer.id}`;
 
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
@@ -23,19 +23,20 @@ function photographerFactory(data) {
         pTagline.setAttribute('class','tagline');
         const pPrice = document.createElement("p");
         pPrice.setAttribute("class","price");
-        location.innerText = photographer._city+", "+photographer._country;
-        pTagline.innerText = photographer._tagline;
-        pPrice.innerText = photographer._price+"€/jour";
-        h2.textContent = photographer._name;
-        linkPhotographer.appendChild(article);
-        article.appendChild(img);
+        location.innerText = photographer.city+", "+photographer.country;
+        pTagline.innerText = photographer.tagline;
+        pPrice.innerText = photographer.price+"€/jour";
+        h2.textContent = photographer.name;
+        linkPhotographer.appendChild(img);
+        linkPhotographer.appendChild(h2)
+
+        article.appendChild(linkPhotographer);
         article.appendChild(divInfosPhotographer);
-        divInfosPhotographer.appendChild(h2);
         divInfosPhotographer.appendChild(location);
         divInfosPhotographer.appendChild(pTagline);
         divInfosPhotographer.appendChild(pPrice);
 
-        return (linkPhotographer);
+        return (article);
     }
     return { getUserCardDOM }
 }
